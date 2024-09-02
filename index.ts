169eeb06dff0edc9e0b3ed09cd45aa0ccd53f6c7
Ta-Run <tarunsharma11091999@gmail.com>
2024-09-02 18:04:38 +0530

    comment lodash

--- index.ts
+++ index.ts
@@ -1,13 +1,13 @@
 import { APIGatewayProxyEventV2, Handler, APIGatewayProxyResultV2 } from 'aws-lambda';
-import * as _ from 'lodash'
+// import * as _ from 'lodash'
 
 export const handler: Handler = async (event: APIGatewayProxyEventV2):Promise<APIGatewayProxyResultV2> => {
     const max = 10;
-    const val = _.random(max);
+    // const val = _.random(max);
 
     const resposne = {
         statusCode: 200,
-        body: `the random value ${val}`
+        body: `the random value ${max}`
     }
 
     return resposne
