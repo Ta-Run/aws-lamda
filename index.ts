efc211376ccfb10f999531a17d8fa9b448027a98
Ta-Run <tarunsharma11091999@gmail.com>
2024-09-02 17:53:30 +0530

    resolve issue for npm install

--- index.ts
+++ index.ts
@@ -1,13 +1,13 @@
-import {APIGatewayProxyEvent,Handler, APIGatewayProxyResultV2} from 'aws-lambda';
+import { APIGatewayProxyEventV2, Handler, APIGatewayProxyResultV2 } from 'aws-lambda';
 import * as _ from 'lodash'
 
-export const handler:Handler = async (event:APIGatewayProxyEvent)=>{
+export const handler: Handler = async (event: APIGatewayProxyEventV2):Promise<APIGatewayProxyResultV2> => {
     const max = 10;
     const val = _.random(max);
 
-    const resposne={
-     statusCode : 200,
-     body: `the random value ${val}`
+    const resposne = {
+        statusCode: 200,
+        body: `the random value ${val}`
     }
 
     return resposne
---
+++ run.ts
@@ -0,0 +1,10 @@
+import  {handler}  from "./index";
+
+const main =async()=>{
+
+    // const res = await handler()
+    console.log(handler)
+
+}
+
+main()
