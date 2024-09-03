33dcae76bf41666094e189f608d77d720d1fe4b9
Ta-Run <tarunsharma11091999@gmail.com>
2024-09-03 11:40:08 +0530

    add zip file of deploy

--- index.ts
+++ index.ts
@@ -1,14 +1,14 @@
 import { APIGatewayProxyEventV2, Handler, APIGatewayProxyResultV2 } from 'aws-lambda';
 // import * as _ from 'lodash'
 
-export const handler: Handler = async (event: APIGatewayProxyEventV2):Promise<APIGatewayProxyResultV2> => {
+export const handler: Handler<APIGatewayProxyEventV2, APIGatewayProxyResultV2> = async (event): Promise<APIGatewayProxyResultV2> => {
     const max = 10;
     // const val = _.random(max);
 
-    const resposne = {
+    const response = {
         statusCode: 200,
-        body: `the random value ${max}`
-    }
+        body: `The random value is ${max}` // Adjusted the message to be more meaningful
+    };
 
-    return resposne
+    return response;
 };
